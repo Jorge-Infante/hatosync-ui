562a3b7093a22356b29e92d01a1caa722d8ce0a4
Jorge-Infante <junior.mk182@gmail.com>
2026-03-21 17:07:32 -0500

    axios, babel

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <template>
   <v-app>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <router-view />
   </v-app>
 </template>
 
-<style>
-nav {
-  padding: 30px;
-}
+<script>
+export default {
+  name: "App",
 
-nav a {
-  font-weight: bold;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
