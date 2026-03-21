0a9f9eb015a133310c6227d0d5558c243fefb99a
Jorge-Infante <junior.mk182@gmail.com>
2026-03-21 14:11:19 -0500

    vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    if (config.optimization.minimizers.has('css')) {
+      config.optimization.minimizers.delete('css')
+    }
+    config.optimization
+      .minimizer('css')
+      .use(CssMinimizerPlugin, [
+        {
+          minimizerOptions: {
+            preset: ['default', { discardDuplicates: false }],
+          },
+        },
+      ])
+  },
 })
